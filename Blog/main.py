e40fbc5fe74a0503c6a5ede366f5cf015d97d407
Harsh-shah10 <harshshah00987@gmail.com>
2024-07-22 11:46:50 +0530

    added changes

--- Blog/main.py
+++ Blog/main.py
@@ -148,6 +148,7 @@ def destroy(id : int, response: Response, db:Session=Depends(get_db)):
         return {"message": f"Blog with id {id} not found"}
     
 
+# Creating a new user
 @app.post('/user')
 def create_user(request: schemas.User,  db:Session=Depends(get_db)):
     user_exist = db.query(models.User).filter(models.User.email==request.email.strip()).first()
@@ -163,6 +164,16 @@ def create_user(request: schemas.User,  db:Session=Depends(get_db)):
     
     db.refresh(new_user)
     return {'message': f'User created successfully with email : {request.email} !!'}
+
+
+# Fetching the particular user
+@app.get("/user/{id}/", status_code=200, response_model=schemas.ShowUser)
+def show_user(id : int, response: Response, db:Session=Depends(get_db)):
+    data = db.query(models.User).filter(models.User.id==id).first()
+    if data:
+        return data
+    else:
+        raise HTTPException(status_code=404, detail=f"User not found with id - {id}")
 
 
 # if __name__ == "__main__":
